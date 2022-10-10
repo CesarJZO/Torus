a07117227a0ae913efd5490ac2208c2275d752c3
CesarJZO <cesarjzo@outlook.com>
2022-10-09 22:54:15 -0500

    Split scene and geometry in different files

--- torus_scene.js
+++ torus_scene.js
@@ -1,10 +1,3 @@
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
 const geometry = new THREE.TorusGeometry(
     10, // Radius 
     4, // Thickness
