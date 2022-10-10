e03b984853c5b7bbd8d93ba2dc58a39bc0c6cf65
CesarJZO <cesarjzo@outlook.com>
2022-10-09 22:03:35 -0500

    Add a new material

--- torus_scene.js
+++ torus_scene.js
@@ -5,11 +5,11 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
-const material = new THREE.MeshBasicMaterial( { color: 0xda964e } );
+const geometry = new THREE.TorusGeometry(10, 4, 50, 60, Math.PI * 2);
+const material = new THREE.MeshNormalMaterial({});
 
-const torus = new THREE.Mesh( geometry, material );
-scene.add( torus );
+const torus = new THREE.Mesh(geometry, material);
+scene.add(torus);
 
 camera.position.z = 30;
 
@@ -17,7 +17,7 @@ function animate() {
     requestAnimationFrame(animate);
 
     torus.rotation.x += 0.01;
-    torus.rotation.y += 0.01;
+    torus.rotation.y += 0.02;
 
     renderer.render(scene, camera);
 };
